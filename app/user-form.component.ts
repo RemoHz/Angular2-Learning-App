0c637f3e2b2d2a2722033ff7d55bc210a5d4db5a
Remo <remocode@gmail.com>
2016-10-04 17:50:30 +1100

    Implement create user to server

--- app/user-form.component.ts
+++ app/user-form.component.ts
@@ -1,17 +1,24 @@
 
 import {Component} from 'angular2/core';
 import {FormBuilder, ControlGroup, Validators} from 'angular2/common';
-import {CanDeactivate} from 'angular2/router';
+import {CanDeactivate, Router} from 'angular2/router';
 
 import {BasicValidators} from './basicValidator';
+import {UserService} from './user.service';
 
 @Component({
-    templateUrl: 'app/user-form.component.html'
+    templateUrl: 'app/user-form.component.html',
+    providers: [UserService]
 })
 export class UserFormComponent implements CanDeactivate {
     form: ControlGroup;
+    isSaving = false;
 
-    constructor(fb: FormBuilder) {
+    constructor(
+        fb: FormBuilder,
+        private _router: Router,
+        private _userService: UserService
+    ) {
         this.form = fb.group({
             name: ['', Validators.required],
             email: ['', BasicValidators.email],
@@ -26,9 +33,16 @@ export class UserFormComponent implements CanDeactivate {
     }
 
     routerCanDeactivate() {
-        if (this.form.dirty)
+        if (this.form.dirty && !this.isSaving)
             return confirm('Are you sure you want to navigate away?');
         
         return true;
+    }
+
+    save() {
+        this._userService.addUser(this.form.value)
+            .subscribe(x => this._router.navigate(['Users']));
+        
+        this.isSaving = true;
     }
 }
