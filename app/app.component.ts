fc0c028f0a3f2f74757a5d60bf2c6e5153fb8fba
Remo <remocode@gmail.com>
2016-10-04 14:55:34 +1100

    Initial Project

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+import {NavBarComponent} from './navbar.component'
+
+@Component({
+    selector: 'my-app',
+    template: '<navbar></navbar>',
+    directives: [NavBarComponent]
+})
+export class AppComponent { }
---
+++ app/home.component.ts
@@ -0,0 +1,9 @@
+
+import {Component, OnInit} from 'angular2/core';
+
+@Component({
+    template: '<h1>Home</h1>'
+})
+export class HomeComponent {
+
+}
